1ebbc9ad1f14717d9a96e6d356d568462e282418
Leho <leho@leho.dev>
2022-03-21 22:10:26 +1100

    upd

--- update_details/update_details.py
+++ update_details/update_details.py
@@ -9,6 +9,7 @@ def update_details(request):
         # privkey = str(os.getenv('FIREBASE_KEYS')) # firebase priv key - linked to service account with perms - doesnt work as firebase sdk needs json file wtf
         privkey = "/keys/firebase_service_key.json" # gets the json secret from mounted disk - manage in gcp secrets manager
         db_url = str(os.getenv("FIREBASE_DB_URL")) # firebase db url - just get from realtime database tab in firebase console
+        print(db_url)
     except Exception as e:
         print("Error Getting env variables!") # this shouldnt ever happen if you have it setup properly
         return_env_variables = {"success" : "false",
@@ -18,26 +19,38 @@ def update_details(request):
 
     cred = credentials.Certificate(privkey) # use actions secrets!!
     try:
-        firebase_admin.initialize_app(cred)
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': "https://musicstreamer-sdd-default-rtdb.asia-southeast1.firebasedatabase.app"  # get db url from secrets for security i guess :/
+        })
     except Exception as e:
         print("firebase app failed to init, or is init already, full traceback: " + str(e))
 
     try:
         args = request.args
         token = args.get("token")
+        nametoset = args.get("name")
         decoded_token = auth.verify_id_token(str(token))
         uid = decoded_token['uid']
-        return str(uid), 200
+
 
     except:
         return_bad_args = {"success" : "false",
              "error" : "bad arguments"}
         return return_bad_args, 400
 
+    try:
+        a = db.reference("/")
+        users_ref = a.child('users')
+        uid_ref = users_ref.child(str(uid))
+        uid_ref.update({'name': str(nametoset)})
+        # users_ref.update({
+        #     str(uid): {
+        #         'name': str(nametoset),
+        #     }
+        # })
+        return "ok", 200
+    except Exception as e:
+        print("Error " + str(e))
 
-    # decoded_token = auth.verify_id_token("")
-    # uid = decoded_token['uid']
-    # print(decoded_token)
-    # print(uid)
 
 
--- user_on_create/user_on_create.py
+++ user_on_create/user_on_create.py
@@ -10,21 +10,24 @@ def user_on_create(event, context): # cloud functions entrypoint
     email = event['email']
     print(str(event))
     try:
-        privkey = str(os.getenv('FIREBASE_KEYS')) # firebase priv key json - linked to service account with perms
+        privkey = "/keys/firebase_service_key.json" # gets the json secret from mounted disk - manage in gcp secrets manager
+        # privkey = str(os.getenv('FIREBASE_KEYS')) # firebase priv key json - linked to service account with perms
         db_url = str(os.getenv("FIREBASE_DB_URL")) # firebase db url - just get from realtime database tab in firebase console
     except Exception as e:
         print("Error Getting Firebase Key!") # this shouldnt ever happen if you have it setup properly
     cred = credentials.Certificate(privkey) # use actions secrets!!
-
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': db_url # get db url from secrets for security i guess :/
-    })
+    try:
+        firebase_admin.initialize_app(cred, {
+            'databaseURL': db_url # get db url from secrets for security i guess :/
+        })
+    except:
+        print("probably init already")
 
     a = db.reference("/")
     users_ref = a.child('users')
-    users_ref.set({
+    users_ref.update({
         str(uid): {
-            'name': '',
+            'name': 'testaccount',
             'email': str(email),
             'profile_picture': 'none',
             'plan': 'free'
