77eb7fa77c618b4ff2de17265b2f9d019a75d111
Leho <leho@leho.dev>
2022-06-03 10:01:36 +1000

    streaming works now via serverless 🐉

--- songs/songs.py
+++ songs/songs.py
@@ -1,6 +1,16 @@
 import json
+import functions_framework
 
 def songs(request, args):
     with open('songs.json', 'r') as f:
         songs = json.load(f)
     return songs
+
+@app.route('/songs', methods=['GET'])
+def get_song():
+    return "songs"
+
+@app.route('/songs/<song_id>', methods=['GET'])
+def getsong(song_id):
+    # stream it back here
+    return "song" + str(song_id)
--- test.py
+++ test.py
@@ -1,17 +1,24 @@
-import flask
-from flask import Flask, request, jsonify
-import json
+from firebase_admin import credentials
+from google.cloud import storage
+from flask import Flask, Response
+from flask import request
+from flask import send_file
 
 app = Flask(__name__)
+# from google.cloud import
+#
+# cred = credentials.Certificate("a.json")  # use actions secrets!!
+@app.route("/test", methods=["GET"])
+def pls():
+    try:
+        storage_client = storage.Client.from_service_account_json('a.json')
+        bucket = storage_client.bucket("music_streamer_songs")
+        blob = bucket.blob("songs/3.mp3")
+        contents = blob.download_as_string()
+        print(contents)
+        return Response(contents, mimetype="audio/mpeg")
+    except Exception as e:
+        print(e)
+        return "wtf are you doing :skull:"
 
-@app.route('/songs', methods=['GET'])
-def get_song():
-    return "songs"
-
-@app.route('/songs/<song_id>', methods=['GET'])
-def getsong(song_id):
-    # stream it back here
-    return "song" + str(song_id)
-
-
-app.run(host='127.0.0.1', port=8080, debug=True)
+app.run(host='0.0.0.0', port=69)
