0858d9090124b02d4adbf25f1b231f006b992129
Leho <leho@leho.dev>
2022-06-15 11:51:34 +1000

    forgot to import something

--- songs/songs.py
+++ songs/songs.py
@@ -1,21 +1,23 @@
 import firebase_admin
-import flask
+# import flask
 import functions_framework
 import json
-from flask import Flask, Response
+# from flask import Flask, Response
 from firebase_admin import auth
 from flask import send_file
-from flask import request
+from flask import Response
+# from flask import request
 import os
 from firebase_admin import credentials
 from firebase_admin import db
 from google.cloud import storage
 
-app = flask.Flask(__name__)
+# app = flask.Flask(__name__)
+
 
 def getobject(id):
     try:
-        storage_client = storage.Client.from_service_account_json('/Users/leho/Documents/GitHub/MusicStreamer-functions/a.json')
+        storage_client = storage.Client.from_service_account_json('/keys/firebase_service_key.json')
         bucket = storage_client.bucket("music_streamer_songs")
         blob = bucket.blob("songs/" + id + ".mp3")
         contents = blob.download_as_string()
@@ -26,29 +28,30 @@ def getobject(id):
         return "wtf are you doing :skull:"
 
 
-
-@app.route("/getsong/", methods=['GET'])
-def songs():
-    try: # shamelessly ripped from oncreate function
-        privkey = "/Users/leho/Documents/GitHub/MusicStreamer-functions/a.json" # gets the json secret from mounted disk - manage in gcp secrets manager
-        db_url = str(os.getenv("FIREBASE_DB_URL")) # firebase db url - just get from realtime database tab in firebase console
+@functions_framework.http
+def songs(request):
+    try:  # shamelessly ripped from oncreate function
+        privkey = "/keys/firebase_service_key.json"  # gets the json secret from mounted disk - manage in gcp secrets manager
+        db_url = str(
+            os.getenv("FIREBASE_DB_URL"))  # firebase db url - just get from realtime database tab in firebase console
         print(db_url)
     except Exception as e:
-        print("Error Getting env variables!") # this shouldnt ever happen if you have it setup properly
-        return_env_variables = {"success" : "false",
-             "error" : "error getting env variables"}
+        print("Error Getting env variables!")  # this shouldnt ever happen if you have it setup properly
+        return_env_variables = {"success": "false",
+                                "error": "error getting env variables"}
         return return_env_variables, 400
     try:
-        cred = credentials.Certificate(privkey) # use actions secrets!!
+        cred = credentials.Certificate(privkey)  # use actions secrets!!
     except Exception as e:
         print("Error loading credentials!")
-        return_credentials = {"success" : "false",
-             "error" : "error loading credentials"}
+        return_credentials = {"success": "false",
+                              "error": "error loading credentials"}
         return return_credentials, 400
 
     try:
         firebase_admin.initialize_app(cred, {
-            'databaseURL': "https://musicstreamer-sdd-default-rtdb.asia-southeast1.firebasedatabase.app"  # get db url from secrets for security i guess :/
+            'databaseURL': "https://musicstreamer-sdd-default-rtdb.asia-southeast1.firebasedatabase.app"
+            # todo fix the bug when getting this db url from secrets manager
         })
     except Exception as e:
         print("firebase app failed to init, or is init already, full traceback: " + str(e))
@@ -56,7 +59,7 @@ def songs():
     headers = request.headers
     songid = headers.get('songid')
     if songid is None:
-        return {"success" : "false", "error" : "no song id"}, 400
+        return {"success": "false", "error": "no song id"}, 400
     else:
         try:
             authtoken = headers.get("Authorization")
@@ -67,7 +70,7 @@ def songs():
 
                 decoded_token = auth.verify_id_token(authtoken)
                 # token is good
-                uid = decoded_token['uid'] # get uid from jwt
+                uid = decoded_token['uid']  # get uid from jwt
                 # get the song from gcp bucket
                 song_probably = getobject(songid)
                 return Response(song_probably, mimetype="audio/mpeg")
@@ -81,4 +84,5 @@ def songs():
             return "Error"
         return "how tf did u get here"
 
-app.run(host="0.0.0.0", port=8080)
+
+# app.run(host="0.0.0.0", port=8080)
